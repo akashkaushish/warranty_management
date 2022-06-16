b963db0ec95f08cbe88e7a773090dd0acc5cf831
Raj Rohit <rajrohit30@gmail.com>
2022-06-16 19:33:11 +0530

    User Seeder Update

--- database/seeders/CreateUsersSeeder.php
+++ database/seeders/CreateUsersSeeder.php
@@ -15,16 +15,26 @@ class CreateUsersSeeder extends Seeder
     {
         $user = [
             [
-               'name'=>'Admin',
-               'email'=>'admin@gmail.com',
-                'is_admin'=>'1',
-               'password'=> bcrypt('admin@123'),
+				'name'=>'Admin',
+				'email'=>'admin@gmail.com',
+				'is_admin'=>'1',
+				'company_name'=>'miger',
+				'address'=>'Via Croce Rossa 11,Ghilarza, Oristano',
+				'website'=>'http://miger.com',
+				'phone_number'=>'+18965424785',
+				'mobile_number'=>'+178964644',
+				'password'=> bcrypt('admin@123'),
             ],
             [
-               'name'=>'Raj',
-               'email'=>'raj@gmail.com',
-                'is_admin'=>'0',
-               'password'=> bcrypt('123456'),
+				'name'=>'Raj',
+				'email'=>'raj@gmail.com',
+				'is_admin'=>'0',
+				'company_name'=>'panamemory',
+				'address'=>'Via del Pontiere 57,Vallo Torinese, Torino',
+				'website'=>'http://panamemory.com',
+				'phone_number'=>'148744545',
+				'mobile_number'=>'1897486484',
+				'password'=> bcrypt('123456'),
             ],
         ];
   
