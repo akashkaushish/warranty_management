5dfdc67475e55afaa612094bfbd8ddb58e780e41
Raj Rohit <rajrohit30@gmail.com>
2022-06-18 12:54:27 +0530

    Customer Listing 635 Task

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,19 +14,23 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-			$table->string('company_name');
-			$table->string('address');
-			$table->string('website')->nullable();
-			$table->string('mobile_number')->nullable();
-			$table->string('user_key');
-            $table->boolean('is_admin')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+				$table->id();
+				$table->string('name');
+				$table->string('email')->unique();
+				$table->timestamp('email_verified_at')->nullable();
+				$table->string('company_name');
+				$table->string('address');
+				$table->string('website')->nullable();
+				$table->string('mobile_number')->nullable();
+				$table->string('user_key');
+				$table->boolean('is_db_created')->default(0);
+				$table->boolean('is_repo_created')->default(0);
+				$table->boolean('is_env_configured')->default(0);
+				$table->string('setup_url')->nullable();
+				$table->boolean('is_admin')->nullable();
+				$table->string('password');
+				$table->rememberToken();
+				$table->timestamps();
         });
     }
 
